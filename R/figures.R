50a00b6a9f5b0b91c6b82f3842c23477154540f3
sangeeta <sangeetabhatia03@gmail.com>
2020-07-02 16:59:57 +0100

    Fix dependencies

--- R/figures.R
+++ R/figures.R
@@ -8,28 +8,27 @@
 ##' @return ggplot object
 ##' @author Sangeeta Bhatia
 ##' @export
-##' @import ggdist
+##' @import ggdist ggplot2
 plot_projections <- function(obs, pred) {
 
   ## Make sure dates columns are actually dates
   obs$dates <- as.Date(obs$dates)
   pred$dates <- as.Date(pred$dates)
-  week_ending <- min(pred$dates)
   ##xintercept <- as.numeric(as.Date(week_ending)) - 0.5
-  p <- ggplot2::ggplot() +
-    ggplot2::geom_point(data = obs, aes(dates, deaths)) +
+  p <- ggplot() +
+    geom_point(data = obs, aes(dates, deaths)) +
     ggdist::geom_lineribbon(
       data = pred,
       aes(
         x = dates, y = val, ymin = .lower, ymax = .upper
       )
     ) +
-    ggplot2::scale_fill_brewer(palette = "Greens") +
-    ggplot2::scale_x_date(limits = c(as.Date("2020-03-01"), NA)) +
+    scale_fill_brewer(palette = "Greens") +
+    scale_x_date(limits = c(as.Date("2020-03-01"), NA)) +
     ##ggplot2::geom_vline(xintercept = xintercept, linetype = "dashed") +
-    ggplot2::theme_minimal() +
-    ggplot2::xlab("") + ggplot2::ylab("Deaths") +
-    ggplot2::theme(legend.position = "none")
+    theme_minimal() +
+    xlab("") + ylab("Deaths") +
+    theme(legend.position = "none")
 
   p
 }
