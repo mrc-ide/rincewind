5db2f74e67b161d0d9b98ead4747258625d85e30
sangeeta <sangeetabhatia03@gmail.com>
2021-01-20 16:37:41 +0000

    IFR calculation moved into package

--- R/irf.R
+++ R/irf.R
@@ -1,23 +1,28 @@
 ##' @importFrom rlang .data
 population_age_pyramid <- function(x) {
-  x <- mutate_at(x, vars(`0-4`:`100+`), as.numeric)
+  x <- dplyr::mutate_at(x, vars(`0-4`:`100+`), as.numeric)
   out <- data.frame(
-    location = location,
-    "[0-15)" = rowSums(select(x, .data$`0-4`:.data$`10-14`)),
-    "[15-44)" = rowSums(select(x, .data$`15-19`:.data$`40-44`)),
-    "[45-64)" = rowSums(select(x, .data$`45-49`:.data$`60-64`)),
-    "[65-74)" = rowSums(select(x, .data$`65-69`:.data$`70-74`)),
-    "75+" = rowSums(select(x, .data$`75-79`:.data$`100+`)),
-    ## total = rowSums(select(x, `0-4`:`100+`)),
+    location = x$Location,
+    age_group = c("[0-15)", "[15-44)", "[45-64)", "[65-74)", "75+"),
+    population = c(
+      rowSums(x[1, c("0-4", "5-9", "10-14")]),
+      rowSums(
+        x[1, c("15-19", "20-24", "25-29", "30-34", "35-39", "40-44")]
+      ),
+      rowSums(x[1, c("45-49", "50-54", "55-59")]),
+      rowSums(x[1, c("65-69", "70-74")]),
+      rowSums(
+        x[1, c("75-79", "80-84", "85-89", "90-94", "95-99", "100+")]
+      )
+    ),
     check.names = FALSE
   )
-  out <- gather(out, .data$age_group, .data$pop, -.data$location)
-  out$prop <- out$pop / sum(out$pop)
+  out$prop <- out$pop / sum(out$population)
   out
 }
 
 
-clean_pop_by_age <- function(pop_by_age) {
+clean_pop_by_age <- function(pop_by_age, locations) {
   colnames(pop_by_age) <- pop_by_age[1, ]
   pop_by_age <- pop_by_age[-1, ]
   ## First re-organise the population in the desired age brakets
@@ -40,15 +45,14 @@ clean_pop_by_age <- function(pop_by_age) {
   df <- pop_by_age[pop_by_age$Location == "Serbia", ]
   df$Location <- "Kosovo"
   pop_by_age <- rbind(pop_by_age, df)
-
-  pop_by_age
+  pop_by_age[pop_by_age$Location %in% locations, ]
 }
 
 
 ### Age-disaggregated IFR
 ### https://www.medrxiv.org/content/10.1101/2020.08.12.20173690v2.full.pdf
 ### Table 3, page 19
-ifr_by_age <- function() {
+ifr_by_age <- function(nsim = 1e4) {
 
   infections <- data.frame(
     age_group = c("[15-44)", "[45-64)", "[65-74)", "75+"),
@@ -64,15 +68,16 @@ ifr_by_age <- function() {
     age_group = c("[15-44)", "[45-64)", "[65-74)", "75+"),
     deaths = c(524, 4657, 5663, 19330)
   )
-  ifr_distr <- map(
+  ifr_distr <- sapply(
     infections$age_group,
-    function(age_group) {
+    FUN = function(age_group) {
       mu <- infections$mu[infections$age_group == age_group]
       sigma <- infections$sigma[infections$age_group == age_group]
-      x <- rnorm(1e4, mean = mu, sd = sigma)
-      while (any(x < 0)) x <- rnorm(1e4, mean = mu, sd = sigma)
+      x <- rnorm(nsim, mean = mu, sd = sigma)
+      while (any(x < 0)) x <- rnorm(nsim, mean = mu, sd = sigma)
       c19_deaths$deaths[c19_deaths$age_group == age_group] / x
-    }
+    },
+    simplify = FALSE
   )
   names(ifr_distr) <- infections$age_group
   ifr_distr
@@ -84,31 +89,34 @@ ifr_by_age <- function() {
 ##' @title Age-weighted IFR of COVID-19
 ##' @param locations list of country names. Note that no checks have
 ##' been implemented to test that the country names are valid.
-##' Use names as in ECDC COVID-19 data, so United_Kingdom instead of
-##' any other variation.
+##'
+##'
 ##' @return a named list with samples from age-weighted IFR for each
 ##' location
 ##' @author Sangeeta Bhatia
 ##' @export
 age_wtd_ifr <- function(locations) {
   ifr_distr <- ifr_by_age()
-  pop_by_age <- clean_pop_by_age(population_by_age)
+  pop_by_age <- clean_pop_by_age(population_by_age, locations)
   locations <- setNames(locations, locations)
-  pop_pyramid <- map(
+  pop_pyramid <- sapply(
     locations,
     function(location) {
       x <- pop_by_age[pop_by_age$Location == location, ]
       population_age_pyramid(x)
-    }
+    },
+    simplify = FALSE
   )
-  out <- map(
+
+  out <- sapply(
     pop_pyramid,
     function(pop) {
-      out <- slide(
+      out <- slider::slide(
         pop, ~ ifr_distr[[as.character(.x$age_group)]] * .x$prop
       )
       out[[5]] + out[[2]] + out[[3]] + out[[4]]
-    }
+    },
+    simplify = FALSE
   )
   out
 }
